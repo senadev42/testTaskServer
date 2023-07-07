4a06fe4fac9027db7d3c0f5a0e075edb2b0e6132
senadev42 <senadevv42@gmail.com>
2023-07-07 12:18:23 +0300

    error handling middleware

---
+++ server/middleware/errorMiddleware.js
@@ -0,0 +1,26 @@
+//For routes that don't exist
+const notFound = (req, res, next) => {
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404);
+  next(error);
+};
+
+//For errors in routes
+const errorHandler = (err, req, res, next) => {
+  //set status code and error message
+  let statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  let message = err.message;
+
+  // If Mongoose not found error, set to 404 and change message
+  if (err.name === "CastError" && err.kind === "ObjectId") {
+    statusCode = 404;
+    message = "Resource not found";
+  }
+
+  res.status(statusCode).json({
+    message: message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+};
+
+export { notFound, errorHandler };
