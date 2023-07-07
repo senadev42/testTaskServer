1b5d7af173e29636e362c960727e15669bdf0451
senadev42 <senadevv42@gmail.com>
2023-07-07 12:18:56 +0300

    added user routes, error middleware, dotenv

--- server/server.js
+++ server/server.js
@@ -1,8 +1,27 @@
-const express = require("express");
-const port = 5000;
+import express from "express";
+import { config } from "dotenv";
 
+//Routes
+import userRoutes from "./routes/userRoutes.js";
+
+//Middleware
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
+
+config();
+
+//setting up the express app
 const app = express();
+const port = process.env.PORT || 5000;
 
+//binding endpoints to routes
+app.use("/api/users", userRoutes);
+
+//error handling middleware
+app.use(notFound);
+app.use(errorHandler);
+
+//home route
 app.get("/", (req, res) => res.send("API running"));
 
+//listening to the port
 app.listen(port, () => console.log(`Server started on port ${port}`));
