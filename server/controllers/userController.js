aa6f815d1598725455ab9b6ae073432f6cb1535c
senadev42 <senadevv42@gmail.com>
2023-07-07 13:56:44 +0300

    token generation in register route

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -35,14 +35,14 @@ const registerUser = asyncHandler(async (req, res) => {
     throw new Error("Username already exists");
   }
 
-  generateToken(res, user._id);
-
   //otherwise create user
   const user = await User.create({
     name,
     email,
     password,
   });
+
+  generateToken(res, user._id);
 
   if (user) {
     res.status(201).json({
