e6b8745ee48703005761509eaa93e6f7ced1adae
senadev42 <senadevv42@gmail.com>
2023-07-07 13:17:58 +0300

    added body parsing middleware to server.js

--- server/server.js
+++ server/server.js
@@ -1,20 +1,24 @@
 import "./config/config.js";
 import express from "express";
 
-//Routes
+//Importing Routes
 import userRoutes from "./routes/userRoutes.js";
 
-//Middleware
+//Importing Middleware
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 
 //setting up the express app
 const app = express();
 const port = process.env.PORT || 5000;
 
-//binding endpoints to routes
+// Body Parsing Middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// Binding Endpoints to Routes
 app.use("/api/users", userRoutes);
 
-//error handling middleware
+// Error Handling Middleware
 app.use(notFound);
 app.use(errorHandler);
 
