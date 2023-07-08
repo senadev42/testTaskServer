2f076f63495c48f4cd1249db6b6c0a0993fce398
senadev42 <senadevv42@gmail.com>
2023-07-08 21:57:11 +0300

    explore controller get nearby cities

---
+++ server/controllers/exploreController.js
@@ -0,0 +1,24 @@
+import asyncHandler from "express-async-handler";
+
+import { getCitiesNearMe } from "../GeoDB/citiesNearMe.js";
+
+/**
+ * Recieve a co-ordinate, and return a list of places nearby
+ *
+ * @route   POST /api/explore/nearby-cities
+ * @access  Private
+ */
+const getNearbyCities = asyncHandler(async (req, res) => {
+  const { coords, _id } = req.body;
+
+  const cities = await getCitiesNearMe(coords);
+
+  console.log("/api/explore/nearby-cities invoked");
+  console.log(`Recieved co-ordinate ${coords} for ${_id}`);
+
+  console.log(cities);
+
+  res.send(cities);
+});
+
+export { getNearbyCities };
