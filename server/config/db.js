ed0a54996f68ac5ef01970f6889ef069144498ab
senadev42 <senadevv42@gmail.com>
2023-07-09 16:07:05 +0300

    added country data routes

--- server/config/db.js
+++ server/config/db.js
@@ -5,7 +5,7 @@ config();
 const connectToMDB = async () => {
   try {
     const conn = await mongoose.connect(process.env.MONGO_URI);
-    console.log(`MongoDB Connected: ${conn.connection.host}`);
+    console.log(`MongoDB Connected: ${conn.connection.host} \n`);
   } catch (error) {
     console.error(`Error: ${error.message}`);
     process.exit(1);
--- server/routes/exploreRoutes.js
+++ server/routes/exploreRoutes.js
@@ -5,10 +5,13 @@ import { protect } from "../middleware/authMiddleware.js";
 const router = express.Router();
 
 import { getNearbyCities } from "../controllers/exploreController.js";
+import { getCountryData } from "../controllers/exploreController.js";
 
 //Parent route - /explore
 
 // 1. Nearby Cities
 router.route("/nearby-cities").post(protect, getNearbyCities);
+// 2. Get Country Data
+router.route("/country-data").get(protect, getCountryData);
 
 export default router;
