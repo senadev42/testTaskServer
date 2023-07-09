7c375c2c1cd30f9494bc21d7cc2e99f070e6987c
senadev42 <senadevv42@gmail.com>
2023-07-09 14:36:33 +0300

    city details util

---
+++ server/GeoDB/cityDetails.js
@@ -0,0 +1,44 @@
+import "../env_config.js";
+import axios from "axios";
+
+//this is a function that will accept an ISO3166 country code
+//and return details about the country
+
+const basegeoURL = "https://wft-geo-db.p.rapidapi.com/v1/geo/";
+
+export async function getCountryDetails(countryCode) {
+  //logging
+  console.log("\nFunction invoked: Get Country Details\n");
+  console.log("Making request to " + basegeoURL + "countries/" + countryCode);
+
+  //test setup
+  console.log("Code: " + countryCode);
+
+  const options = {
+    method: "GET",
+    url: basegeoURL + "countries/" + countryCode,
+    headers: {
+      "X-RapidAPI-Key": process.env.RAPID_API_KEY,
+      "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+    },
+  };
+
+  let results = [];
+  try {
+    //1. Fire off requests
+    const response = await axios.request({
+      ...options,
+    });
+
+    // 1.5 Add the results to the array
+
+    results = response.data;
+  } catch (error) {
+    console.error(error.response.data);
+  }
+
+  return results;
+}
+
+const testcoord = "UA";
+console.log(await getCountryDetails(testcoord));
