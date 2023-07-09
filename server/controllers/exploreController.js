786010a2ad9240388786d5b2ee1e7eeab5e05a94
senadev42 <senadevv42@gmail.com>
2023-07-09 16:07:29 +0300

    get country data controller

--- server/controllers/exploreController.js
+++ server/controllers/exploreController.js
@@ -1,6 +1,9 @@
 import asyncHandler from "express-async-handler";
+import CountrySearch from "../models/countrySearchQueryModel.js";
 
+//geodb utility functions
 import { getCitiesNearMe } from "../GeoDB/citiesNearMe.js";
+import { getCountryDetails } from "../GeoDB/countryData.js";
 
 /**
  * Recieve a co-ordinate, and return a list of places nearby
@@ -23,4 +26,33 @@ const getNearbyCities = asyncHandler(async (req, res) => {
   res.send(cities);
 });
 
-export { getNearbyCities };
+/**
+ * Recieve an iso Country Code and return information about that country
+ *
+ * @route   POST /api/explore/country-data
+ * @access  Private
+ */
+const getCountryData = asyncHandler(async (req, res) => {
+  console.log(`Get country data invoked by ${req.ip} `);
+  const { countryCode, query_user_id } = req.body;
+
+  let countryData = await getCountryDetails(countryCode);
+
+  console.log("\nRecieved:" + countryCode);
+  console.log("From: " + query_user_id);
+
+  //return request data to user
+  res.send(countryData);
+
+  console.log(
+    `Adding ${countryCode} country search  by ${query_user_id} to the database ...`
+  );
+  //log country search query
+  const countrySearch = await CountrySearch.create({
+    countryCode,
+    query_user_id,
+  });
+  console.log("Added country search to the database.");
+});
+
+export { getNearbyCities, getCountryData };
