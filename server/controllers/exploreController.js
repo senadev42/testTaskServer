026cb486ee7059352842be69cb52b99e9edad7a4
senadev42 <senadevv42@gmail.com>
2023-07-10 07:32:57 +0300

    refactored get Country Data

--- server/controllers/exploreController.js
+++ server/controllers/exploreController.js
@@ -10,8 +10,11 @@ import { getCountryDetails } from "../GeoDB/countryData.js";
  *
  * @route   POST /api/explore/nearby-cities
  * @access  Private
+ * @expects coords, _id
+ * @returns cities: list of places nearby
  */
 const getNearbyCities = asyncHandler(async (req, res) => {
+  //why do we need _id exactly? to store user location?
   const { coords, _id } = req.body;
 
   console.log("Recieved:" + coords);
@@ -31,28 +34,40 @@ const getNearbyCities = asyncHandler(async (req, res) => {
  *
  * @route   POST /api/explore/country-data
  * @access  Private
+ * @expects countryCode, query_user_id
+ * @returns countryData: details about a country code
  */
 const getCountryData = asyncHandler(async (req, res) => {
   console.log(`Get country data invoked by ${req.ip} `);
-  const { countryCode, query_user_id } = req.body;
 
-  let countryData = await getCountryDetails(countryCode);
+  //1. get data from request body
+  const { countryCode, query_user_id } = req.body;
 
   console.log("\nRecieved:" + countryCode);
   console.log("From: " + query_user_id);
 
-  //return request data to user
-  res.send(countryData);
+  //2. invoke geodb utility function
+  let countryData = await getCountryDetails(countryCode);
 
+  //To the Database
   console.log(
     `Adding ${countryCode} country search  by ${query_user_id} to the database ...`
   );
-  //log country search query
-  const countrySearch = await CountrySearch.create({
-    countryCode,
-    query_user_id,
-  });
-  console.log("Added country search to the database.");
+  let CountrySearchObject = {
+    query_user_id: query_user_id,
+    countryCode: countryData.data.code,
+    countryName: countryData.data.name,
+    countryCapital: countryData.data.capital,
+    numRegions: countryData.data.numRegions,
+    flagUri: countryData.data.flagImageUri,
+    callingCode: countryData.data.callingCode,
+  };
+  console.log(CountrySearchObject);
+  const countrySearch = await CountrySearch.create(CountrySearchObject);
+  console.log("Added a country search to the database.");
+
+  //4. return request data to user
+  res.send(countryData);
 });
 
 export { getNearbyCities, getCountryData };
