5111d621e0bff831be0d0c61b887a260ea5c3160
senadev42 <senadevv42@gmail.com>
2023-07-10 15:27:16 +0300

    cookie test - samesite none

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -38,7 +38,7 @@ const authUser = asyncHandler(async (req, res) => {
     res.cookie("testtaskcookie", token, {
       httpOnly: true,
       secure: process.env.NODE_ENV !== "development",
-      sameSite: "strict",
+      sameSite: "none",
       maxAge: 30 * 24 * 60 * 60 * 1000,
     });
 
@@ -94,7 +94,7 @@ const registerUser = asyncHandler(async (req, res) => {
   res.cookie("testtaskcookie", token, {
     httpOnly: true,
     secure: process.env.NODE_ENV !== "development",
-    sameSite: "strict",
+    sameSite: "none",
     maxAge: 30 * 24 * 60 * 60 * 1000,
   });
 
